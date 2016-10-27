af657f847edb96e85c687baefae01d457c528b91
Arthur Schneider <dev.schneiart@gmail.com>
2016-10-27 22:25:21 +0200

    Format the file so it looks prettier

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,15 +3,21 @@ import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule }    from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }        from './app.component';
 import { UserDetailComponent } from './user-detail.component';
-import { UsersComponent } from './users.component';
-import { UserService } from './user.service';
+import { UsersComponent }      from './users.component';
+import { UserService }         from './user.service';
+import { UserSearchComponent } from './user-search.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpModule ],
-  declarations: [ AppComponent, UsersComponent,  UserDetailComponent ],
+  declarations: [
+                  AppComponent,
+                  UsersComponent,
+                  UserDetailComponent,
+                  UserSearchComponent
+                ],
   providers:    [ UserService ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent, UserSearchComponent ]
 })
 export class AppModule { }
